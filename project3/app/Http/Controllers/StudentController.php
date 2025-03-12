f2d40865d5449964fef6e61162c5b8b1f5aa3686
cayman718 <a44362652@gmail.com>
2025-03-12 15:45:56 +0800

    mvc練習

---
+++ project3/app/Http/Controllers/StudentController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Student;
+use App\Models\Phone;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // dd('student index ok');
+        // $users = DB::table('users')->get();
+        // $data = DB::table('students')->get();
+        // $data = Student::get();
+
+        // $phone = User::find(1)->phone;
+        $data = Student::with('phone')->with('hobbies')->get();
+        // dd($data[0]->phone);
+        // dd($data);
+
+        // $data foreach
+        foreach ($data as $key => $value1) {
+            $tmp = [];
+            foreach ($value1->hobbies as $value2) {
+                // dd($value2->name);
+                array_push($tmp, $value2->name);
+            }
+            $tmpString = implode(',', $tmp);
+        }
+        // dd($tmp);
+        dd($tmpString);
+        return view('student.index', ['data' => $data]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        // dd('student controller create');
+        return view('student.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        // dd($request);
+        $input = $request->except('_token');
+        // dd($input);
+
+        // 主表
+        $data = new Student;
+        $data->name = $input['name'];
+        $data->mobile = $input['mobile'];
+        $data->save();
+
+        // 子表
+        $item = new Phone;
+        $item->student_id = $data->id;
+        $item->phone = $input['phone'];
+        $item->save();
+
+
+        return redirect()->route('students.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        $data = Student::where('id', $id)->with('phone')->first();
+
+        return view('student.edit', ['data' => $data]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $input = $request->except('_token', '_method');
+
+        //主表
+        $data = Student::where('id', $id)->first();
+        $data->name = $input['name'];
+        $data->mobile = $input['mobile'];
+        $data->save();
+
+        //子表
+        // 刪除子表
+        Phone::where('student_id', $id)->delete();
+        // 新增子表
+        $item = new Phone;
+        $item->student_id = $data->id;
+        $item->phone = $input['phone'];
+        $item->save();
+
+
+        return redirect()->route('students.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        // 刪除子表
+        Phone::where('student_id', $id)->delete();
+        // 刪除主表
+        Student::where('id', $id)->delete();
+
+        return redirect()->route('students.index');
+    }
+
+    public function excel()
+    {
+        dd('hello student controller excel');
+    }
+
+    public function sayHello()
+    {
+        dd('hello kai');
+    }
+}
---
+++ project3/app/Models/Student.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class Student extends Model
+{
+    /**
+     * Get the phone associated with the user.
+     */
+    public function phone(): HasOne
+    {
+        return $this->hasOne(Phone::class);
+    }
+
+    /**
+     * Get the phone associated with the user.
+     */
+    public function hobbies(): HasMany
+    {
+        return $this->hasMany(Hobby::class);
+    }
+}
